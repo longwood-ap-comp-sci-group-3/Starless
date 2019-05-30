007f77465e879f46afb8eb5cf5f072571f2b17db
Murizath <epicdude6126@gmail.com>
2019-05-30 09:40:27 -0400

    Adding libraries.

--- src/Draw.java
+++
@@ -1,8 +0,0 @@
-
-public class Draw {
-Draw() {
-	
-}
-
-
-}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
 public static void main(String [] args) {
-	Game Starless = new Game();
-	new Thread(Starless).start();
+
 }
 }
