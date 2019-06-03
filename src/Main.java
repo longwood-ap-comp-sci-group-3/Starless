5fe3b1e67e30712a38573491240f1e99e2ed50d5
Murizath <epicdude6126@gmail.com>
2019-06-03 10:00:27 -0400

    W.I.P class. Learning Slick!

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,44 @@
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 
-public class Main extends BasicGame
+public class Main extends StateBasedGame
 {
-  public Main(String title)
+	
+	public static final String gamename = "Starless";
+	public static final int menu = 0;
+	public static final int play = 1;
+	
+	
+  public Main(String gamename)
   {
-    super(title);
+    super(gamename);
+    this.addState(new Menu(menu));
+    this.addState(new Play(play));
   }
   
   public static void main(String[] args) throws SlickException
   {
-    AppGameContainer app = null;
-    app = new AppGameContainer(new Main("Starless"));
-    app.setDisplayMode(800, 600, false);
-    app.start();
+    AppGameContainer appgc;
+    try {
+    	appgc = new AppGameContainer(new Main(gamename));
+    }catch(SlickException e) {
+    	e.printStackTrace();
+    }
+    appgc = new AppGameContainer(new Main("Starless"));
+    appgc.setDisplayMode(800, 600, false);
+    appgc.start();
   }
-  
-  public void render(GameContainer arg0, org.newdawn.slick.Graphics arg1)
-    throws SlickException
-  {}
-  
-  public void init(GameContainer arg0)
-    throws SlickException
-  {}
-  
-  public void update(GameContainer arg0, int arg1)
-    throws SlickException
-  {}
+
+@Override
+public void initStatesList(GameContainer gc) throws SlickException {
+	this.getState(menu).init(gc,  this);
+	this.getState(play).init(gc,  this);
+	this.enterState(menu);
+	
+}
+
 }
